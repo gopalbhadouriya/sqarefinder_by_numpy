f77721d55f295f3688a35d06c64ee34f428382fc
Gopalbhadouriya <gopalbhadouriya32@gmail.com>
2024-10-17 13:57:34 +0530

    Add files via upload

---
+++ sqarefinder.py
@@ -0,0 +1,18 @@
+# get sqare using numpy
+import numpy as np
+
+def getsqare():
+    a = int(input("Enter your number to check sqare: "))
+
+    ar = np.array(a)
+
+    c = np.square(ar)
+    print(c)
+
+
+getsqare()
+getsqare()
+getsqare()
+getsqare()
+getsqare()
+getsqare()
